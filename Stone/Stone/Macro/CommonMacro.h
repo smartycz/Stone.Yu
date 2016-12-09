287b067a2dda485032c2e02e461e7b962d529ada
yuchenggui <yuchenggui@58ganji.com>
2016-12-09 18:30:26 +0800

    add CommonMacro.h

---
+++ Stone/Stone/Macro/CommonMacro.h
@@ -0,0 +1,13 @@
+//
+//  CommonMacro.h
+//  Stone
+//
+//  Created by Stone.Yu on 2016/12/9.
+//  Copyright © 2016年 Stone.Yu. All rights reserved.
+//
+
+#ifndef CommonMacro_h
+#define CommonMacro_h
+
+
+#endif /* CommonMacro_h */
