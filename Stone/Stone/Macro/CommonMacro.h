c8c65d10a0ef69c516e2ff7809dba5da9107136a
yuchenggui <yuchenggui@58ganji.com>
2016-12-09 18:40:02 +0800

    update CommonMacro.h

--- Stone/Stone/Macro/CommonMacro.h
+++ Stone/Stone/Macro/CommonMacro.h
@@ -9,5 +9,12 @@
 #ifndef CommonMacro_h
 #define CommonMacro_h
 
+// 获取屏幕 宽度、高度
+#define Screen_Width                        ([UIScreen mainScreen].bounds.size.width)
+#define Screen_Height                       ([UIScreen mainScreen].bounds.size.height)
+
+// 定义 weakSelf
+#define WEAK_SELF                           __weak __typeof(self)weakSelf = self;
+#define STRONG_SELF                         __strong __typeof(weakSelf)self = weakSelf;
 
 #endif /* CommonMacro_h */
